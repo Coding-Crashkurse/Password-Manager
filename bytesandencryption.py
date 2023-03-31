aa8c7773c83e73bfd3f3fa0f8820be1a6212bf45
markus <datamastery87@gmail.com>
2023-03-31 20:40:53 +0200

    add encrypt

--- bytesandencryption.py
+++ bytesandencryption.py
@@ -1,5 +1,6 @@
 import hashlib
 from base64 import urlsafe_b64encode
+from cryptography.fernet import Fernet
 
 key = "test"
 key = key.encode()
@@ -9,3 +10,7 @@ key = hashlib.sha256(key).digest()
 print(key)
 
 print(urlsafe_b64encode(key))
+
+fernet_key = Fernet.generate_key()
+x = Fernet(fernet_key).decrypt("test")
+print(x)
